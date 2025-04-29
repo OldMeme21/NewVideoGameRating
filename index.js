882a5e9f2142e0e444a400ebaf4813f8c804cf1b
Dylan Filla <130706676+OldMeme21@users.noreply.github.com>
2025-04-28 20:48:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
 
 async function runExample() {
 
-    var x = new Float32Array( 1, 32 )
+    var x = new Float32Array( 1, 24 )
 
     var x = [];
 
-     x[0] = document.getElementById('console').value;
-     x[1] = document.getElementById('alcohol_reference').value;
-     x[2] = document.getElementById('animated_blood').value;
-     x[3] = document.getElementById('blood').value;
-     x[4] = document.getElementById('blood_and_gore').value;
-     x[5] = document.getElementById('cartoon_violence').value;
-     x[6] = document.getElementById('crude_humor').value;
-     x[7] = document.getElementById('drug_reference').value;
-     x[8] = document.getElementById('fantasy_violence').value;
-     x[9] = document.getElementById('intense_violence').value;
-     x[10] = document.getElementById('language').value;
-     x[11] = document.getElementById('lyrics').value;
-     x[12] = document.getElementById('mature_humor').value;
-     x[13] = document.getElementById('mild_blood').value;
-     x[14] = document.getElementById('mild_cartoon_violence').value;
-     x[15] = document.getElementById('mild_fantasy_violence').value;
-     x[16] = document.getElementById('mild_language').value;
-     x[17] = document.getElementById('mild_lyrics').value;
-     x[18] = document.getElementById('mild_suggestive_themes').value;
-     x[19] = document.getElementById('mild_violence').value;
-     x[20] = document.getElementById('no_descriptors').value;
-     x[21] = document.getElementById('nudity').value;
-     x[22] = document.getElementById('partial_nudity').value;
-     x[23] = document.getElementById('sexual_content').value;
-     x[24] = document.getElementById('sexual_themes').value;
-     x[25] = document.getElementById('simulated_gambling').value;
-     x[26] = document.getElementById('strong_janguage').value;
-     x[27] = document.getElementById('strong_sexual_content').value;
-     x[28] = document.getElementById('suggestive_themes').value;
-     x[29] = document.getElementById('use_of_alcohol').value;
-     x[30] = document.getElementById('use_of_drugs_and_alcohol').value;
-     x[31] = document.getElementById('violence').value;
+    x[0] = document.getElementById('alcohol_reference').value;
+    x[1] = document.getElementById('animated_blood').value;
+    x[2] = document.getElementById('blood').value;
+    x[3] = document.getElementById('blood_and_gore').value;
+    x[4] = document.getElementById('cartoon_violence').value;
+    x[5] = document.getElementById('crude_humor').value;
+    x[6] = document.getElementById('drug_reference').value;
+    x[7] = document.getElementById('fantasy_violence').value;
+    x[8] = document.getElementById('intense_violence').value;
+    x[9] = document.getElementById('language').value;
+    x[10] = document.getElementById('lyrics').value;
+    x[11] = document.getElementById('mature_humor').value;
+    x[12] = document.getElementById('mild_blood').value;
+    x[13] = document.getElementById('mild_cartoon_violence').value;
+    x[14] = document.getElementById('mild_fantasy_violence').value;
+    x[15] = document.getElementById('mild_language').value;
+    x[16] = document.getElementById('mild_lyrics').value;
+    x[17] = document.getElementById('mild_suggestive_themes').value;
+    x[18] = document.getElementById('mild_violence').value;
+    x[19] = document.getElementById('no_descriptors').value;
+    x[20] = document.getElementById('nudity').value;
+    x[21] = document.getElementById('partial_nudity').value;
+    x[22] = document.getElementById('sexual_content').value;
+    x[23] = document.getElementById('sexual_themes').value;
 
-    let tensorX = new onnx.Tensor(x, 'float32', [1, 32]);
+    let tensorX = new onnx.Tensor(x, 'float32', [1, 24]);
 
     let session = new onnx.InferenceSession();
 
